62f0224fe2ac33b8207227508ab48c7f49f5de6b
Evan Klitzke <evan@eklitzke.org>
2018-02-20 11:46:52 -0500

    Try to use posix_fadvise with CBufferedFile
    This primarily affects blocks when bitcoin is launched with -reindex, as
    that causes the block files to be loaded as CBufferedFile objects one at
    a time as the reindex progresses.
    
    Co-Authored-By: Luke Dashjr <luke-jr+git@utopios.org>
    
    Github-Pull: #14485
    Rebased-From: 289e88b3133107f8a54184d8316559c7cf8ddb12

--- src/streams.h
+++ src/streams.h
@@ -11,6 +11,7 @@
 #include <span.h>
 #include <support/allocators/zeroafterfree.h>
 #include <util/check.h>
+#include <util/fs_helpers.h>
 #include <util/obfuscation.h>
 #include <util/overflow.h>
 #include <util/syserror.h>
@@ -441,6 +442,11 @@ public:
     /** Wrapper around TruncateFile(). */
     bool Truncate(unsigned size);
 
+    void AdviseSequential()
+    {
+        ::AdviseSequential(m_file);
+    }
+
     //! Write a mutable buffer more efficiently than write(), obfuscating the buffer in-place.
     void write_buffer(std::span<std::byte> src);
 
@@ -529,11 +535,18 @@ public:
     {
         if (nRewindIn >= nBufSize)
             throw std::ios_base::failure("Rewind limit must be less than buffer size");
+        m_src.AdviseSequential();
     }
 
     ~BufferedFile() { fclose(); }
 
-    int fclose() { return m_src.fclose(); }
+    int fclose()
+    {
+        if (auto rel{m_src.release()}) {
+            return CloseAndUncache(rel);
+        }
+        return m_src.fclose();
+    }
 
     //! check whether we're at the end of the source file
     bool eof() const {
