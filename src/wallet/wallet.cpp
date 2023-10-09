3a9b6cea1ba74d48a1e1231cb50287366f6f9b6e
Sjors Provoost <sjors@sprovoost.nl>
2023-10-09 11:19:14 +0200

    wallet: track background validation height
    Github-Pull: #28616
    Rebased-From: 3f3dbb60d190e76074ec2da7b303d68f89fe28ee

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1479,11 +1479,22 @@ void CWallet::transactionRemovedFromMempool(const CTransactionRef& tx, MemPoolRe
 
 void CWallet::blockConnected(ChainstateRole role, const interfaces::BlockInfo& block)
 {
-    if (role == ChainstateRole::BACKGROUND) {
-        return;
-    }
     assert(block.data);
     LOCK(cs_wallet);
+
+    switch (role) {
+        case ChainstateRole::BACKGROUND:
+            m_background_validation_height = block.height;
+            return;
+        case ChainstateRole::ASSUMEDVALID:
+            if (m_background_validation_height == -1) {
+                m_background_validation_height = 0;
+            }
+            break;
+        case ChainstateRole::NORMAL:
+            m_background_validation_height = -1;
+            break;
+    } // no default case, so the compiler can warn about missing cases
 
     // Update the best block in memory first. This will set the best block's height, which is
     // needed by MarkConflicted.
