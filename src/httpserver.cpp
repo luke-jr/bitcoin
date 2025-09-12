bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Bugfix: httpserver: Close listen socket if we fail to make an evhttp handle

src/httpserver.cpp
@@ -363,9 +363,12 @@ my_bind_socket_with_handle(struct evhttp *http, const char *address, ev_uint16_t
     if (aitop) evutil_freeaddrinfo(aitop);
 
     bound = evhttp_accept_socket_with_handle(http, fd);
-    if (bound != nullptr) {
-        LogPrint(BCLog::LIBEVENT, "libevent: Bound to port %d - Awaiting connections ... \n", port);
+    if (bound == nullptr) {
+        evutil_closesocket(fd);
+        return nullptr;
     }
+
+    LogPrint(BCLog::LIBEVENT, "libevent: Bound to port %d - Awaiting connections ... \n", port);
     return bound;
 }
 

luke-jr/bitcoin

src/httpserver.cpp
@@ -427,9 +427,12 @@ my_bind_socket_with_handle(struct evhttp *http, const char *address, ev_uint16_t
     if (aitop) evutil_freeaddrinfo(aitop);
 
     bound = evhttp_accept_socket_with_handle(http, fd);
-    if (bound != nullptr) {
-        LogDebug(BCLog::LIBEVENT, "libevent: Bound to port %d - Awaiting connections ... \n", port);
+    if (bound == nullptr) {
+        evutil_closesocket(fd);
+        return nullptr;
     }
+
+    LogDebug(BCLog::LIBEVENT, "libevent: Bound to port %d - Awaiting connections ... \n", port);
     return bound;
 }
 