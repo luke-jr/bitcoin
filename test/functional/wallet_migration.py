b88763c056096dc0db65f59ae3fecce4892030fe
Luke Dashjr <luke-jr+git@utopios.org>
2026-01-20 05:03:31 +0000

    QA: wallet_migration: Test ".", "./", "..", and "../subdir"

--- test/functional/wallet_migration.py
+++ test/functional/wallet_migration.py
@@ -1710,7 +1710,11 @@ class WalletMigrationTest(BitcoinTestFramework):
 
         migration_failure_cases = [
             "",
+            ".",
+            "./",
+            "..",
             "../",
+            "../subdir",
             os.path.abspath(self.master_node.datadir_path / "absolute_path"),
             "normallynamedwallet"
         ]
