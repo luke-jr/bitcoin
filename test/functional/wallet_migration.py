18ada0717d10df2b25b12a7c9c6574a3881c502e
Luke Dashjr <luke-jr+git@utopios.org>
2026-01-21 15:02:52 +0000

    QA: wallet_migration: Test various other relative paths

--- test/functional/wallet_migration.py
+++ test/functional/wallet_migration.py
@@ -1738,6 +1738,10 @@ class WalletMigrationTest(BitcoinTestFramework):
             "..",
             "../",
             "../subdir",
+            "../subdir/..",
+            "subdir/two",
+            "subdir/../two",
+            "subdir/two/..",
             os.path.abspath(self.master_node.datadir_path / "absolute_path"),
             "normallynamedwallet"
         ]
