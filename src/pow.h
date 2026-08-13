bfac46a1017b6832eec88e696598d516fb280f84
Luke Dashjr <luke-jr+git@utopios.org>
2026-08-13 16:18:16 +0000

    pow: Use ARG_NONNULL for GetNextWorkRequired assumptions (and fix fuzz to comply)

--- src/pow.h
+++ src/pow.h
@@ -6,6 +6,7 @@
 #ifndef BITCOIN_POW_H
 #define BITCOIN_POW_H
 
+#include <attributes.h>
 #include <consensus/params.h>
 
 #include <stdint.h>
@@ -26,7 +27,7 @@ class arith_uint256;
  */
 std::optional<arith_uint256> DeriveTarget(unsigned int nBits, const uint256 pow_limit);
 
-unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params&);
+unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params&) ARG_NONNULL(1) ARG_NONNULL(2);
 unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params&);
 
 /** Check whether a block hash satisfies the proof-of-work requirement specified by nBits */
